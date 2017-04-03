17d0aec4b4b83c7509d29d27081d15be25de4035
kinmanz <kinmanz42695>
2017-04-03 19:11:02 +0300

    test

---
+++ src/test/java/GitHR/Services/GitHubServiceTest.java
@@ -0,0 +1,82 @@
+package GitHR.Services;
+
+import com.google.gson.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.*;
+import java.util.concurrent.Future;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GitHubServiceTest {
+
+    ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private GitHubService gitHubService = new GitHubService(threadPoolTaskExecutor);
+    private PropertyService propertyService = new PropertyService();
+
+    @BeforeClass
+    public static void runOnceBeforeClass() {
+        GitHubService.TESTED = true;
+    }
+
+    @Before
+    public void runOnceBeforeInstance() {
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        threadPoolTaskExecutor.initialize();
+        gitHubService.setToken(propertyService.getProperty("test.token"));
+    }
+
+    @Test
+    public void getAuthenticatedUserTest() throws Exception {
+        JsonObject jsonObject = gitHubService.getAuthenticatedUser();
+        System.out.println(gson.toJson(jsonObject));
+
+        jsonObject = gitHubService.getUser("ALEXSSS");
+        System.out.println(gson.toJson(jsonObject));
+
+        System.out.println(jsonObject.get("login"));
+        System.out.println(jsonObject.get("login").toString().replaceAll("^\"|\"$", ""));
+
+    }
+
+    @Test
+    public void getGraphQLUserInfoTest() throws Exception {
+        JsonObject jsonObject = gitHubService.getProfileInfoGraphQL("ALEXSSS");
+        System.out.println(gson.toJson(jsonObject));
+    }
+
+    @Test
+    public void getRepoContributors() throws Exception {
+        JsonArray jsonArray = gitHubService.getRepoContributers("d3/d3");
+        System.out.println(gson.toJson(jsonArray));
+    }
+
+    @Test
+    public void getRepoContributorsByLogin() throws Exception {
+        JsonObject jsonObject = gitHubService.getRepoContributionForProfile("d3/d3", "mbostock");
+        System.out.println(gson.toJson(jsonObject));
+    }
+
+
+    @Test
+    public void getFullCvJSON() throws Exception {
+        JsonObject jsonObject = gitHubService.getFullCvJSON("ALEXSSS");
+        System.out.println(gson.toJson(jsonObject));
+    }
+
+    @Test
+    public void getFullCvJSONMock() throws Exception {
+        JsonObject jsonObject = gitHubService.getFullCvJSONMock("ALEXSSS");
+        System.out.println(gson.toJson(jsonObject));
+    }
+}
+
+
+
